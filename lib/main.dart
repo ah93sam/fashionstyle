d0637c7d23cefcb3248541f5244ca28f9a98bfe4
ALaa Sami <alaasami@ALaas-MacBook-Pro.local>
2020-08-13 03:42:46 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:fashionstyle/provider/app.dart';
+import 'package:fashionstyle/provider/product.dart';
+import 'package:fashionstyle/provider/user.dart';
+import 'package:fashionstyle/screens/home.dart';
+import 'package:fashionstyle/screens/login.dart';
+import 'package:fashionstyle/screens/splash.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider.value(value: UserProvider.initialize()),
+    ChangeNotifierProvider.value(value: ProductProvider.initialize()),
+    ChangeNotifierProvider.value(value: AppProvider()),
+
+
+  ], child: MaterialApp(
+    debugShowCheckedModeBanner: false,
+    theme: ThemeData(
+        primaryColor: Colors.white
+    ),
+    home: ScreensController(),
+  ),));
+}
+
+class ScreensController extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context);
+    switch(user.status){
+      case Status.Uninitialized:
+        return Splash();
+      case Status.Unauthenticated:
+      case Status.Authenticating:
+        return Login();
+      case Status.Authenticated:
+        return HomePage();
+      default: return Login();
+    }
+  }
+}
